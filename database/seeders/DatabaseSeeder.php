046c2ec652161236c925d2aab2d28a093055a645
MehAk <mehakarmaan1@gmail.com>
2026-06-16 14:13:49 +0400

    Make database seeder valid

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,19 +7,11 @@ use App\Models\HomepageSection;
 use App\Models\MediaAsset;
 use App\Models\NavigationItem;
 use App\Models\SiteSetting;
-use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        User::updateOrCreate(
-            ['email' => 'admin'.'@'.'studybuddy.local'],
-            ['name' => 'StudyBuddy Admin', 'password' => Hash::make('Change'.'Me12345!'), 'role' => 'admin', 'is_admin' => true]
-        );
-
-        $settings = [
-            ['site_name','StudyBuddy','text','identity'], ['brand_name','StudyBuddy','text','identity'],
-            ['logo_path','assets
+        foreach ([
+            ['brand_name', 'Study
