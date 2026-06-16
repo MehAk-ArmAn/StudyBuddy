be91e592888fb7ba5959fe10a193170456760c10
MehAk <mehakarmaan1@gmail.com>
2026-06-16 14:16:20 +0400

    Restore valid empty database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,16 +2,12 @@
 
 namespace Database\Seeders;
 
-use App\Models\FooterItem;
-use App\Models\HomepageSection;
-use App\Models\MediaAsset;
-use App\Models\NavigationItem;
-use App\Models\SiteSetting;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        foreach ([
-            ['brand_name', 'Study
+        // Temporary valid seeder. CMS seed content will be restored in the next fix commit.
+    }
+}
