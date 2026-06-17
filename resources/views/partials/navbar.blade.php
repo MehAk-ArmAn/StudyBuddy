1c2d9d967a24e260ae5d5d793343b07a38539976
Mehak <mehakarmaan1@gmail.com>
2026-06-17 09:55:43 +0400

    Fix navbar Blade parsing

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,1 +1,33 @@
-<nav class="nav"><a class="brand" href="{{ route('home') }}">@if(!empty($settings['logo_path']))<img src="{{ asset($settings['logo_path']) }}" alt="{{ $settings['brand_name'] ?? 'StudyBuddy' }}">@endif<span>{{ $settings['brand_name'] ?? 'StudyBuddy' }}</span></a><button class="nav-toggle" data-nav-toggle>Menu</button><div class="nav-links" data-nav-links>@foreach($navigationItems as $item)<a href="{{ $item->url }}" @if($item->opens_new_tab) target="_blank" rel="noopener" @endif>{{ $item->label }}</a>@endforeach@if(!empty($settings['global_cta_label']))<a class="nav-cta" href="{{ $settings['global_cta_url'] ?? '#top' }}">{{ $settings['global_cta_label'] }}</a>@endif</div></nav>
+﻿<nav class="nav">
+    <a class="brand" href="{{ route('home') }}">
+        @if (!empty($settings['logo_path']))
+            <img src="{{ asset($settings['logo_path']) }}" alt="{{ $settings['brand_name'] ?? 'StudyBuddy' }}">
+        @endif
+
+        <span>{{ $settings['brand_name'] ?? 'StudyBuddy' }}</span>
+    </a>
+
+    <button class="nav-toggle" type="button" data-nav-toggle>
+        Menu
+    </button>
+
+    <div class="nav-links" data-nav-links>
+        @foreach ($navigationItems as $item)
+            @if ($item->opens_new_tab)
+                <a href="{{ $item->url }}" target="_blank" rel="noopener">
+                    {{ $item->label }}
+                </a>
+            @else
+                <a href="{{ $item->url }}">
+                    {{ $item->label }}
+                </a>
+            @endif
+        @endforeach
+
+        @if (!empty($settings['global_cta_label']))
+            <a class="nav-cta" href="{{ $settings['global_cta_url'] ?? '#top' }}">
+                {{ $settings['global_cta_label'] }}
+            </a>
+        @endif
+    </div>
+</nav>
