9e1058aeee4a8949ef70ab4ba0900a1f30b0f13d
MehAk <mehakarmaan1@gmail.com>
2026-06-17 15:56:38 +0400

    Support nested page CMS forms

--- resources/views/admin/resources/form.blade.php
+++ resources/views/admin/resources/form.blade.php
@@ -1,1 +1,45 @@
-@extends('layouts.admin')@section('content')<h2>{{ $title }}</h2>@if($errors->any())<div class="error"><ul>@foreach($errors->all() as $error)<li>{{ $error }}</li>@endforeach</ul></div>@endif<form class="editor" method="POST" action="{{ $method==='POST' ? (isset($parent) ? route($route.'.store', $parent) : route($route.'.store')) : (isset($parent) ? route($route.'.update', [$parent, $item]) : route($route.'.update', $item)) }}">@csrf @if($method!=='POST')@method($method)@endif @foreach($fields as $field)<label>{{ $field }}@if(in_array($field,['is_enabled','opens_new_tab','is_active']))<input type="checkbox" name="{{ $field }}" value="1" @checked(old($field, $item->$field ?? true))>@elseif(str_contains($field,'body')||str_contains($field,'subtitle')||$field==='value'||$field==='settings')<textarea name="{{ $field }}">{{ old($field, is_array($item->$field) ? json_encode($item->$field) : $item->$field) }}</textarea>@else<input name="{{ $field }}" value="{{ old($field, $item->$field) }}" @if(str_contains($field,'url')) type="url" @else type="text" @endif>@endif</label>@endforeach <button>Save</button><a class="button" href="{{ isset($parent) ? route($route.'.index', $parent) : route($route.'.index') }}">Cancel</a></form>@endsection
+@extends('layouts.admin')
+
+@section('content')
+    @php
+        $parentParams = $parentParams ?? (isset($parent) ? (is_array($parent) ? $parent : [$parent]) : []);
+        $formAction = $method === 'POST'
+            ? route($route.'.store', $parentParams)
+            : route($route.'.update', array_merge($parentParams, [$item]));
+    @endphp
+
+    <h2>{{ $title }}</h2>
+
+    @if($errors->any())
+        <div class="error">
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form class="editor" method="POST" action="{{ $formAction }}">
+        @csrf
+        @if($method !== 'POST')
+            @method($method)
+        @endif
+
+        @foreach($fields as $field)
+            <label>
+                {{ $field }}
+                @if(in_array($field, ['is_enabled', 'opens_new_tab', 'is_active']))
+                    <input type="checkbox" name="{{ $field }}" value="1" @checked(old($field, $item->$field ?? true))>
+                @elseif(str_contains($field, 'body') || str_contains($field, 'subtitle') || str_contains($field, 'description') || $field === 'value' || $field === 'settings')
+                    <textarea name="{{ $field }}">{{ old($field, is_array($item->$field) ? json_encode($item->$field) : $item->$field) }}</textarea>
+                @else
+                    <input name="{{ $field }}" value="{{ old($field, $item->$field) }}" @if(str_contains($field, 'url')) type="url" @else type="text" @endif>
+                @endif
+            </label>
+        @endforeach
+
+        <button>Save</button>
+        <a class="button" href="{{ route($route.'.index', $parentParams) }}">Cancel</a>
+    </form>
+@endsection
